e86f79e833b1cec1689fc424581c405286e7f55c
shelly0011 <shelly1224r@gmail.com>
2020-09-03 19:00:38 -0400

    Update Sz.jl

--- Sz.jl
+++ Sz.jl
@@ -3,11 +3,13 @@ module Sz
         # returns size of matrix as (rows, columns)
         mat_pair = size(matrix)[1]
         return mat_pair
-    end
+    end  
+
     function c(matrix)
         mat_pair = size(matrix)[2]
         return mat_pair
     end
+
     function z(matrix)
         z = (r(matrix) == 0)
     end
